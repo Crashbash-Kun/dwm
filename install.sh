e15d0b944aa68f4e3497d90d1fb6953054165b88
Crashbash-Kun <crashbash0@gmail.com>
2019-05-30 13:42:10 -0500

    Switched to using hardlink for .xinitrc, added setting wallpaper using bgs, fixed keybinds.

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo make clean install
 # Backup old xinit, just in case
 mv -i ~/.xinitrc ~/.xinitrc.old
 
-# Symbolic link back to the xinit so that changes from git apply
-cp -s .xinitrc ~/.xinitrc
+# Link back to the xinit so that changes from git apply
+ln ./.xinitrc ~/.xinitrc
